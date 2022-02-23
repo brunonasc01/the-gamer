443559a66d6c7f58e76ea9fdd20ac2cb243aad1c
Bruno Araujo <bruno.araujo@itlab.com.br>
2022-02-22 21:53:29 -0300

    project structure

---
+++ src/main/java/br/com/method/the/gamer/TheGamerApplication.java
@@ -0,0 +1,13 @@
+package br.com.method.the.gamer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheGamerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheGamerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/method/the/gamer/TheGamerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.method.the.gamer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheGamerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
